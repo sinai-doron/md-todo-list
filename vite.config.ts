19e76d0b04d075c3cdd1f17a47b55fbd60cc9c6a
Doron Sinai <sinai.doron+github@gmail.com>
2026-01-10 01:15:11 +0200

    SEO support

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import prerender from 'vite-plugin-prerender'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    prerender({
-      routes: ['/', '/visualizer'],
-      renderer: '@prerenderer/renderer-puppeteer',
-      rendererOptions: {
-        renderAfterDocumentEvent: 'render-complete',
-        maxConcurrentRoutes: 2,
-        headless: true,
-      },
-    }),
-  ],
+  plugins: [react()],
   build: {
     rollupOptions: {
       output: {
